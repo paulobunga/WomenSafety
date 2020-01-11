8906b0d72130840b43e49950de44b02f88ab71a1
error-yatish <dhananiyatish@gmail.com>
2020-01-11 10:14:32 +0530

    Localization and background location fetch

--- App.tsx
+++ App.tsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import {
-  Provider as PaperProvider,
-  ActivityIndicator,
-  Colors
-} from "react-native-paper";
+import { Provider as PaperProvider, Button } from "react-native-paper";
 import { VoiceRecorder, Geolocation } from "./src/components";
 import { setUpBackgroundLocationTask } from "./src/utils";
-
+import "./src/utils/localization";
+import { useTranslation } from "react-i18next";
 setUpBackgroundLocationTask();
 
 export default function App() {
+  const changeLanguage = () => {
+    i18n.changeLanguage("gu");
+  };
+
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {}, []);
+
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>{t("title")}</Text>
         <VoiceRecorder />
         <Geolocation />
+        <Button onPress={changeLanguage}>Change language</Button>
       </View>
     </PaperProvider>
   );
---
+++ src/utils/localization/index.tsx
@@ -0,0 +1,32 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: {
+        translation: {
+          title: "Welcome"
+        }
+      },
+      gu: {
+        translation: {
+          title: "સ્વાગત છે"
+        }
+      },
+      hi: {
+        translation: {
+          title: "स्वागत हे"
+        }
+      }
+    },
+    lng: "en",
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
